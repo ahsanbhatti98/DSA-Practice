ff2201f63ac422e22e72f654d985fbf90b362960
Tech <techversatile2020@gmail.com>
2024-11-23 20:09:11 +0500

    practice find prime number

---
+++ Mathmatical-Algorithm/PrimeNumber.js
@@ -0,0 +1,30 @@
+// problem with this approach is its only checks for 2
+const primeNumberMyApproach = (n) => {
+  if (n < 2) return false;
+  if (n % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+// console.log(primeNumberMyApproach(103));
+console.log(primeNumberMyApproach(9));
+// console.log(primeNumberMyApproach(4));
+
+const isPrime = (n) => {
+  if (n < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isPrime(3));
+console.log(isPrime(5));
+console.log(isPrime(4));
