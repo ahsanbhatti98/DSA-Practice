ed4d7418123c65524164d3277933552111a80049
Tech <techversatile2020@gmail.com>
2024-11-23 20:07:42 +0500

    practice find  factorial of number

---
+++ Mathmatical-Algorithm/FactorialOfANumber.js
@@ -0,0 +1,20 @@
+const findFactorailMyAppraoch = (n) => {
+  if (n <= 1) {
+    return 1;
+  } else {
+    return n * findFactorailMyAppraoch(n - 1);
+  }
+};
+
+// console.log("=>>>>", findFactorailMyAppraoch(4));
+
+const forLoopFactorailMyAppraoch = (n) => {
+  let result = 1;
+  if (n <= 1) return result;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+};
+
+console.log(forLoopFactorailMyAppraoch(0))
